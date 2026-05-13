d2b779679a0b707011fcba30e21d13d31be8ecb9
0x1kayla <kaylasanchez0920@gmail.com>
2026-05-12 20:12:22 -0700

    omg

--- environ/env03.js
+++ environ/env03.js
@@ -18,3 +18,19 @@ $("#needy-button").click(function () {
     
     let currentMood = buttonCreature.moods[arrayPosition];
 }
+
+
+function askNumber(whatNumber) {
+    let userNumber = prompt("Guess 1-10?");
+
+    if (userNumber == whatNumber) {
+        $("#output").html("You got it!");
+    }
+    else {
+        $("#output").html("Noooooope");
+    }
+}
+
+$("#good-button").click(function () {
+    askNumber(5);
+});
